2b2ac79d2668edd7f5c309b503578a189e4e8ca1
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-02-15 17:14:30 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let formfield = document.getElementById('formfield');
+
+function add() {
+  let newField = document.createElement('input');
+  newField.setAttribute('type', 'text');
+  newField.setAttribute('name', 'text');
+  newField.setAttribute('class', 'text');
+  newField.setAttribute('size', 50);
+  newField.setAttribute('placeholder', 'Optional Field');
+  formfield.appendChild(newField);
+}
